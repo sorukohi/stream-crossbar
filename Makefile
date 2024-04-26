846630758b3b7f7b72e8e9b23500ab6ff7be3f68
Victor Moskvin <gubkaroll@mail.ru>
2024-04-27 02:15:37 +0300

    Feat(Makefile, scripts): Флаг GUI

--- Makefile
+++ Makefile
@@ -1,14 +1,14 @@
-SCRIPTS_PATH = ./scripts
-TMP_PATH = ./tmp
+SCRIPTS_PATH=./scripts
+TMP_PATH=./tmp
 
-TEST = crossbar_3x4
+TEST=crossbar_3x4
+GUI=0
 
 synth:
 	vivado -mode tcl -nolog -nojournal -source $(SCRIPTS_PATH)/synth.tcl 
 
 sim:
-	vivado -mode tcl -nolog -nojournal -source $(SCRIPTS_PATH)/sim.tcl -tclargs $(TEST)
-	# tclsh $(SCRIPTS_PATH)/sim.tcl $(TEST)
+	vivado -mode tcl -nolog -nojournal -source $(SCRIPTS_PATH)/sim.tcl -tclargs $(TEST) $(GUI)
 	
 clean:
 ifeq ($(OS), Windows_NT)
--- scripts/sim.tcl
+++ scripts/sim.tcl
@@ -1,16 +1,22 @@
-if {[llength $argv] != 1} {
+if {[llength $argv] != 2} {
 	puts stderr "Usage: $argv0 <condition>"
 	exit 1
 }
-lassign $argv test_name
+lassign $argv test_name gui
 
 set source_files [glob "./rtl/*.sv"]
 
-if {$test_name == "crossbar_3x3"} {
-	set tb_files ./tb/tb_stream_xbar_3x3.sv
-} else {
+if {$test_name == "crossbar_3x4"} {
 	set tb_files ./tb/tb_stream_xbar_3x4.sv
-} 
+} elseif {$test_name == "crossbar_3x3"} {
+	set tb_files ./tb/tb_stream_xbar_3x3.sv
+} elseif {$test_name == "crossbar_1x1"} {
+	set tb_files ./tb/tb_stream_xbar_1x1.sv
+}
+
+if {$gui == 1} {
+	start_gui
+}
 
 create_project simulation ./tmp/sim_output
 
